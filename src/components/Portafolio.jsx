001ab009479d1c599d13f241b72c71c7c8426a8f
nico <nico.bernard2207@gmail.com>
2023-08-18 16:24:12 -0300

    portfolio

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -10,67 +10,7 @@ const Portafolio= () => {
           </h2>
 
           <div className='portfolio__container container grid'>
-            <div className="slider">
-              <div>
-                <img src="/public/img/ecom.png" alt="" />
-                <h3 className='modal__title'>e-commerce</h3>
-                <ul className='modal__list grid'>
-                  <li className='modal__item'>
-                    <span className='item__icon'><i className="fa-regular fa-file-lines"></i></span>
-                
-                    <div>
-                      <span className='item__title'>proyecto</span>
-                      <span className='item__details'>e-commerce</span>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-
-              <div>
-                <img src="/public/img/pokedex.png" alt="" />
-                <h3 className='modal__title'>pokedex</h3>
-                <ul className='modal__list grid'>
-                  <li className='modal__item'>
-                    <span className='item__icon'><i className="fa-regular fa-file-lines"></i></span>
-                
-                    <div>
-                      <span className='item__title'>proyecto</span>
-                      <span className='item__details'>pokedex</span>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-
-              <div>
-                <img src="/public/img/ricky.png" alt="" />
-                <h3 className='modal__title'>rick and morty Locations</h3>
-                <ul className='modal__list grid'>
-                  <li className='modal__item'>
-                    <span className='item__icon'><i className="fa-regular fa-file-lines"></i></span>
-                
-                    <div>
-                      <span className='item__title'>proyecto</span>
-                      <span className='item__details'>rick and morty Locations</span>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-
-              <div>
-                <img src="/public/img/weather.png" alt="" />
-                <h3 className='modal__title'>weather page</h3>
-                <ul className='modal__list grid'>
-                  <li className='modal__item'>
-                    <span className='item__icon'><i className="fa-regular fa-file-lines"></i></span>
-                
-                    <div>
-                      <span className='item__title'>proyecto</span>
-                      <span className='item__details'>weather page</span>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            
           </div>
             
         </section>
