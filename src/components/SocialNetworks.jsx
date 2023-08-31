14672e66c67bd20f669c4d4ec925990228ea8f22
nico <nico.bernard2207@gmail.com>
2023-08-31 00:48:50 -0300

    portfolio

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -3,10 +3,21 @@ import "../styles/socialNetworks.css"
 const SocialNetworks = () => {
     return(        
         <section className="social__networks_container">
-            <a href="https://wa.me/1133298630"><span><i className="fa-brands fa-whatsapp social_network"></i></span></a>
-            <a href="mailto:nico.bernard2207@gmail.com"><span><i className="fa-regular fa-envelope social_network"></i></span></a>
-            <a href="https://github.com/bernardnico26"><span><i className="fa-brands fa-github social_network"></i></span></a>
-            <a href="https://www.linkedin.com/in/nicolas-bernard-ab4984236/"><span><i className="fa-brands fa-linkedin social_network"></i></span></a>
+
+                <input type="checkbox" id="btn-mas"/>
+                <div className="btn-mas">
+                    <label htmlFor="btn-mas" className="fa fa-plus"></label>
+                </div>
+                <div className="redes">
+                    <a href="https://wa.me/1133298630"><i className="fa-brands fa-whatsapp social_network"></i></a>
+                    <a href="mailto:nico.bernard2207@gmail.com"><i className="fa-regular fa-envelope social_network"></i></a>
+                    <a href="https://github.com/bernardnico26"><i className="fa-brands fa-github social_network"></i></a>
+                    <a href="https://www.linkedin.com/in/nicolas-bernard-ab4984236/"><i className="fa-brands fa-linkedin social_network"></i></a>
+                </div>
+                
+
+            
+            
         </section>
     )
 }
