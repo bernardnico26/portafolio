c5063c69ae43dab7f97c75cee87704b54740a847
nico <nico.bernard2207@gmail.com>
2023-09-02 02:44:41 -0300

    portfolio

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -11,7 +11,7 @@ const SocialNetworks = () => {
                 </div>
                 <div className="redes">
                     <Themes />
-                    <a href="https://wa.me/1133298630" className="links"><i className="fa-brands fa-whatsapp social_network"></i></a>
+                    <a href="https://wa.me/+541126704676" className="links"><i className="fa-brands fa-whatsapp social_network"></i></a>
                     <a href="mailto:nico.bernard2207@gmail.com" className="links"><i className="fa-regular fa-envelope social_network"></i></a>
                     <a href="https://github.com/bernardnico26" className="links"><i className="fa-brands fa-github social_network"></i></a>
                     <a href="https://www.linkedin.com/in/nicolas-bernard-ab4984236/" className="links"><i className="fa-brands fa-linkedin social_network"></i></a>
